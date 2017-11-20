7f24915c7914cade2104781d4cdea8bb82d9a08f
Berkay Antmen <c.berkayantmen@gmail.com>
2017-11-20 00:01:42 -0500

    Added readme, fixed verbose flag, added viz_path flag and scoped visualization to success cases.

---
+++ README.md
@@ -0,0 +1,19 @@
+**Instructions**
+1) Train a simple CNN on mnist by running `python train.py`. You should see an output similar to `Test accuracy 0.9853`. The model is now saved under `./data/mnist_conv/`.
+2) Generate adversarial samples for the model in first step. For example,
+`python gen_adv.py --verbose=0 --victim_class=2 --wanted_class=6 --num_iterations=30 --num_viz_samples=10 --viz_path=adv_grid_viz.png`
+3) View the visual saved at `adv_grid_viz.png`. The first column corresponds to samples from the mnist test set that the classifier predicted correctly, while the second column are adversarially modified versions of the former that were misclassified as the target class and the last column is a grayscale image of the difference between the images in second and first column. Keep in mind that, since black is represented as 0 and white is represented as 1, images of the third column will be darker in positions with smaller differences.
+
+**How**
+- Fast gradient sign method ([fgsm][1]) is normally applied in a single step to create an adversarial version of a sample when the attack is not targeted.
+- However, since we want to create adversarial examples to be confused as a given class, fgsm is applied in small steps via gradient descent manner to minimize the cross entropy of the victim samples with respect to the target class.
+- Values outside of [0, 1] are clipped to make the adversarial examples more interesting.
+
+**TODO**
+- Reproduce [saliency map][2] based approach
+- Look into retraining the targeted net with adversarial examples
+- Look into Hessian (or other curvature) based targeted attacks
+- Research/think of different attack methods
+
+[1]:https://arxiv.org/abs/1412.6572
+[2]:https://arxiv.org/abs/1511.07528
--- gen_adv.py
+++ gen_adv.py
@@ -8,15 +8,23 @@ import numpy as np
 from collections import Counter
 from model import get_convnet_tensors, add_adversarial_segment
 
+
 def parse_args():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--is_verbose', default=True, help='Whether logging is verbose or not.')
-    parser.add_argument('--victim_class', default=2, help='Victim class of the adversarial attack.')
-    parser.add_argument('--wanted_class', default=6, help='Class to misclassify the victim samples for.')
-    parser.add_argument('--num_iterations', default=30, help='Maximum number of iterations used for optimizing adversarial examples.')
-    parser.add_argument('--num_viz_samples', default=10, help='Number of samples to visualize.')
+    parser.add_argument('--verbose', default=1, type=int, help='Whether logging is verbose (1) or not (0).')
+    parser.add_argument('--victim_class', default=2, type=int, help='Victim class of the adversarial attack.')
+    parser.add_argument('--wanted_class', default=6, type=int, help='Class to misclassify the victim samples for.')
+    parser.add_argument('--num_iterations', default=30, type=int,
+        help='Maximum number of iterations used for optimizing adversarial examples.')
+    parser.add_argument('--num_viz_samples', default=10, type=int, help='Number of samples to visualize.')
+    parser.add_argument('--viz_path', default='adv_grid_viz.png', type=str, help='Path to save the visualization.')
     return parser.parse_args()
+
+MODEL_PATH = 'data/mnist_conv/mnist_conv'
+args = parse_args()
+
+maybe_print = print if args.verbose else lambda *args, **kwargs: None
 
 def pick_k(k, *arrays):
     import random
@@ -51,14 +59,11 @@ def save_visualization(victim_x, modified_x):
         plot(x_diff[i], (i, 2), label='Difference' if i + 1 == num_x else None)
 
     gs.tight_layout(fig)
-    plt.savefig('adv_grid_viz.png')
+    plt.savefig(args.viz_path)
+    maybe_print('Figure is saved at', args.viz_path)
 
-MODEL_PATH = 'data/mnist_conv/mnist_conv'
-args = parse_args()
-
-maybe_print = print if args.is_verbose else lambda *args, **kwargs: None
-
-maybe_print('Will create adversarial samples of {} to be misclassified as {}'.format(args.victim_class, args.wanted_class))
+maybe_print(
+    'Will create adversarial samples of {} to be misclassified as {}'.format(args.victim_class, args.wanted_class))
 
 with tf.Session() as sess:
     saver = tf.train.import_meta_graph('{}.meta'.format(MODEL_PATH))
@@ -73,6 +78,7 @@ with tf.Session() as sess:
 
     current_class, wanted_class = args.victim_class, args.wanted_class
 
+    # Only pick samples of the victim class that the net was able to classify correctly
     victim_mask = np.argmax(mnist.test.labels, axis=1) == current_class
     victim_mask &= test_pred == current_class
     victim_x, victim_y = mnist.test.images[victim_mask], mnist.test.labels[victim_mask]
@@ -94,6 +100,10 @@ with tf.Session() as sess:
         maybe_print('Iteration:', i, end=', ')
         maybe_print('Misclassification rate:', np.sum(predicted_classes != current_class) / float(num_victims), end=', ')
         maybe_print('Targeted misclassification rate:', np.sum(predicted_classes == wanted_class) / num_victims)
+
+    # Only visualize the succesful adversarial examples
+    success_mask = predicted_classes == wanted_class
+    victim_x, modified_x = victim_x[success_mask], modified_x[success_mask]
 
     maybe_print('Creating and saving the visualizations for {} randomly chosen samples.'.format(args.num_viz_samples))
     save_visualization(*pick_k(args.num_viz_samples, victim_x, modified_x))
